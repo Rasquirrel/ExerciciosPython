c9d8212b6ea6b637843b957e6041ffeab781fb9b
José Isac <jose-0A4@protonmail.com>
2022-07-26 13:25:09 -0300

    🙂  Criei o arquivo Ex041
    Criei o arquivo e adicionei o cabeçalho que eu sempre adiciono.

---
+++ MundoDois/ex041.py
@@ -0,0 +1,16 @@
+# Progama desenvolvido por José Isac
+# 26 de julho, 13:20
+
+# Cabeçalho
+
+print('-=' * 50)
+print('DESAFIO N°41')
+print('''A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta 
+e mostre sua categoria, de acordo com a idade:
+– Até 9 anos: MIRIM
+– Até 14 anos: INFANTIL
+– Até 19 anos: JÚNIOR
+– Até 25 anos: SÊNIOR
+– Acima de 25 anos: MASTER
+''')
+print('-=' * 50)
