a132f3509326ba6d4ccccae254e911b51dd8978a
José Isac <jose-0A4@protonmail.com>
2022-07-26 11:16:25 -0300

    🌱 Progresso 1
    Adicionei a primeira parte do programa, onde tem um texto que dá as devidas instruções para o usuário.

--- MundoDois/ex040.py
+++ MundoDois/ex040.py
@@ -9,3 +9,13 @@ print('Crie um progama que leia duas notas de um aluno e calcule sua média, mos
       'no final de acordo com a média atingida. Média abaixo de 5 é reprovado, entre 5 e 6.9 é rec\n'
       'uperação, média 7 ou superior é aprovado.')
 print('-=' * 50)
+print()
+
+# Inicio
+
+print('''Bem vindo ao progama do cálculo de média!
+Irei pedir dois valores de nota.
+Se a média delas for abaixo de 5, retornarei REPROVADO;
+Se a média delas for entre 5 e 6.9, retornarei RECUPERAÇÃO;
+Se a média delas for 7 ou superior, retornarei APROVADO.
+''')
