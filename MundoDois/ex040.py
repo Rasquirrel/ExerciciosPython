670893b50ccfaa881ee521f3f9622509aa79d75f
José Isac <jose-0A4@protonmail.com>
2022-07-26 11:29:47 -0300

    🐿  Versão 1.0
    Adicionei os testes de média e terminei de fazer a primeira versão do código

--- MundoDois/ex040.py
+++ MundoDois/ex040.py
@@ -1,5 +1,6 @@
 # Progama desenvolvido por José Isac
-# 26 de julho, 11:02
+# 26 de julho, 11:29
+# Versão 1.0
 
 # Crie um programa que leia duas notas de um aluno e calcule sua média,
 # mostrando uma mensagem no final, de acordo com a média atingida
@@ -28,3 +29,12 @@ nota_Dois = float(input('DIGITE O VALOR DA SEGUNDA NOTA: '))
 # Calculo da média
 
 media = (nota_Um + nota_Dois) / 2
+
+# Testes
+
+if media >= 7:
+    print(f'SUA MÉDIA É {media} E VOCÊ ESTÁ APROVADO! PARABENS')
+elif 5 < media < 6.9:
+    print(f'SUA MÉDIA É {media} E VOCÊ ESTÁ DE RECUPERAÇÃO! BOA SORTE')
+elif media < 5:
+    print(f'SUA MÉDIA É {media} E VOCÊ ESTÁ REPROVADO! ATÉ A PRÓXIMA!')
