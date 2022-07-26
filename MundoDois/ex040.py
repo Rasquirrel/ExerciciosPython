c939b87ae7b78c136d071ee073bb5bd92c96fac9
José Isac <jose-0A4@protonmail.com>
2022-07-26 11:09:40 -0300

    🙂  Criei o arquivo ex040
    Criei o arquivo e adicionei o cabeçalho que eu sempre faço.

---
+++ MundoDois/ex040.py
@@ -0,0 +1,11 @@
+# Progama desenvolvido por José Isac
+# 26 de julho, 11:02
+
+# Crie um programa que leia duas notas de um aluno e calcule sua média,
+# mostrando uma mensagem no final, de acordo com a média atingida
+print('-=' * 50)
+print('DESAFIO N°40')
+print('Crie um progama que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem\n'
+      'no final de acordo com a média atingida. Média abaixo de 5 é reprovado, entre 5 e 6.9 é rec\n'
+      'uperação, média 7 ou superior é aprovado.')
+print('-=' * 50)
