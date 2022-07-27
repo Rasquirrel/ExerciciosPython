1a3843345bedfe6126689553d4a588969349eaa7
Jose Isac <jose-0A4@protonmail.com>
2022-07-27 08:31:20 -0300

    🌱 Ex043 - Adicionei o cabeçalho no progama

---
+++ MundoDois/ex043.py
@@ -0,0 +1,12 @@
+# Progama desenvolvido por José Isac
+# 27 de julho 8:27
+# Para calcular o IMC eu devo pegar o peso da pessoa em kg e dividir pela altura dela ao quadrado
+
+print('-=' * 50)
+print('DESAFIO N°43')
+print('''Desenvolva um aplicativo que ajude o usuário a calcular o IMC.
+Se o IMC for abaixo de 18,5 - Abaixo do peso
+Se o IMC for entre 18,5 e 25 - Peso ideal
+Se o IMC for entre 30 e 40 - Obesidade
+Se o IMC for acima de 40 - Obesidade Mórbida''')
+print('-=' * 50)
