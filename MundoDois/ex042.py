50a117e47a6a1eb081cf12ae5081883958d7e99e
Jose Isac <jose-0A4@protonmail.com>
2022-07-27 07:53:22 -0300

    🌱 Ex042 - Adicionei os Prompts

--- MundoDois/ex042.py
+++ MundoDois/ex042.py
@@ -8,3 +8,7 @@ print('''Desenvola um aplicativo que ajude o usuário a descobrir se três retas
 formam um triângulo e após isso informe qual o tipo de triângulo.
 ''')
 print('-=' * 50)
+
+reta_A = float(input('Digite o valor da primeira reta: '))
+reta_B = float(input('Digite o valor da segunda reta: '))
+reta_C = float(input('Digite o valor da terceira reta: '))
