86e91d0fc300520c88fd2b9e29e25bcadc1a97fa
Jose Isac <jose-0A4@protonmail.com>
2022-07-27 07:51:29 -0300

    🌱 Ex042 - Adicionei o cabeçalho do progama

---
+++ MundoDois/ex042.py
@@ -0,0 +1,10 @@
+# Progama desenvolvido por José Isac
+# 27 de julho, 7:46
+# Para mim saber se três retas formam um triângulo, a soma de dois lados tem que ser maior que o terceiro lado.
+
+print('-=' * 50)
+print('DESAFIO N°42')
+print('''Desenvola um aplicativo que ajude o usuário a descobrir se três retas
+formam um triângulo e após isso informe qual o tipo de triângulo.
+''')
+print('-=' * 50)
