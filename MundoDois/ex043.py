78b563b9360f3e07d419ded5e6a062c63f6661e9
Jose Isac <jose-0A4@protonmail.com>
2022-07-27 08:40:34 -0300

    🐿 Ex043 - V1.0

--- MundoDois/ex043.py
+++ MundoDois/ex043.py
@@ -1,5 +1,6 @@
 # Progama desenvolvido por José Isac
-# 27 de julho 8:27
+# 27 de julho 8:40
+# Versão 1.0
 # Para calcular o IMC eu devo pegar o peso da pessoa em kg e dividir pela altura dela ao quadrado
 
 print('-=' * 50)
@@ -10,3 +11,20 @@ Se o IMC for entre 18,5 e 25 - Peso ideal
 Se o IMC for entre 30 e 40 - Obesidade
 Se o IMC for acima de 40 - Obesidade Mórbida''')
 print('-=' * 50)
+
+peso = float(int('Informe seu peso em Kg: '))
+altura = float(int('Informe sua altura: '))
+imc = peso / altura**2
+
+if 18.5 > imc:
+    print('Você pesa {}Kg e possui o IMC de {}, o que significa que você tem:')
+    print('ABAIXO DO PESO')
+elif 18.5 < imc < 25:
+    print('Você pesa {}Kg e possui o IMC de {}, o que significa que você tem:')
+    print('PESO IDEAL')
+elif 30 < imc < 40:
+    print('Você pesa {}Kg e possui o IMC de {}, o que significa que você tem:')
+    print('OBESIDADE')
+elif 40 < imc:
+    print('Você pesa {}Kg e possui o IMC de {}, o que significa que você tem:')
+    print('OBESIDADE MÓRBIDA')
