ac034c1b0d2bbf9db0f19ae00d7abc16085eba20
Jose Isac <jose-0A4@protonmail.com>
2022-07-27 08:09:04 -0300

    🐿 Ex042 - V1.0

--- MundoDois/ex042.py
+++ MundoDois/ex042.py
@@ -1,5 +1,6 @@
 # Progama desenvolvido por José Isac
-# 27 de julho, 7:46
+# 27 de julho, 8:08
+# Versão 1.0
 # Para mim saber se três retas formam um triângulo, a soma de dois lados tem que ser maior que o terceiro lado.
 
 print('-=' * 50)
@@ -12,3 +13,13 @@ print('-=' * 50)
 reta_A = float(input('Digite o valor da primeira reta: '))
 reta_B = float(input('Digite o valor da segunda reta: '))
 reta_C = float(input('Digite o valor da terceira reta: '))
+
+if reta_A + reta_B > reta_C and reta_B + reta_C > reta_A and reta_C + reta_A > reta_B:
+    if reta_A == reta_B == reta_C:
+        print('É POSSÍVEL FORMAR UM TRIÂNGULO EQUILÁTERO')
+    elif (reta_A == reta_B) and (reta_A == reta_B) < reta_C:
+        print('É POSSÍVEL FORMAR UM TRIÂNGULO ISÓCELES')
+    else:
+        print('É POSSÍVEL FORMAR UM TRIÂNGULO ESCALENO')
+else:
+    print('NÃO SERÁ POSSÍVEL FORMAR UM TRIÂNGULO')
