76453af4ca99fdfb578437ac6b800914651c3ba1
Jose Isac <jose-0A4@protonmail.com>
2022-07-28 11:41:54 -0300

    Adicionei o cabeçalho que eu sempre adiciono

---
+++ MundoDois/ex045.py
@@ -0,0 +1,13 @@
+# Progama desenvolvido por José Isac
+# 28 de julho, 11:40
+# Pedra papel tesoura
+
+# É um dos primeiros progamas em python que eu desenvolvo enquanto aprendo
+# a utilizar o editor de textos para terminal, VIM.
+
+print('-=' * 40)
+print('DESAFIO N°45')
+print('Desenvolva o jogo Jokenpo')
+print('-=' * 40)
+print()
+
