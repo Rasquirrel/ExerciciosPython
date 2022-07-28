a7937f88a81d89e1af125d539e9424b6fb15858d
Jose Isac <jose-0A4@protonmail.com>
2022-07-28 13:19:08 -0300

    Atualização de melhorias no código do jogo
    Após assistir a aula do curso de python no qual ocorre a resolução desse exercício, eu aprendi uma maneira muito melhor de escrever o código, então resolvi editar o código.

--- MundoDois/ex045.py
+++ MundoDois/ex045.py
@@ -17,7 +17,8 @@ print()
 print('BEM VINDO AO JOGO DE PEDRA PAPEL TESOURA!')
 print('Vamos jogar!')
 
-comp_Option = randint(0, 2)
+Option = ('Pedra', 'Papel', 'Tesoura')
+comp_Option = Option[randint(0, 2)]
 
 print('Vai!')
 print('''[ 0 ] Pedra
@@ -27,52 +28,42 @@ print('''[ 0 ] Pedra
 
 player_Option = int(input())
 
+sleep(1)
 print('Pedra')
-sleep(1.9)
+sleep(1)
 print('Papel')
 sleep(1)
 print('Tesoura!')
 print()
 
-if player_Option == 0 and comp_Option == 0:
-	print('Computador: PEDRA')
-	print('Você: PEDRA')
-	print('Empate! Até a próxima!')
-elif player_Option == 0 and comp_Option == 1:
-	print('Computador: PAPEL')
-	print('Você: PEDRA')
-	print('Você perdeu! Até a próxima!')
-elif player_Option == 0 and comp_Option == 2:
-	print('Computador: TESOURA')
-	print('Você: PEDRA')
-	print('Você ganhou! Até a próxima!')
+# Jogo
+if comp_Option == 'Pedra':
+	if player_Option == 0:
+		print('Computador: PEDRA\nVocê: PEDRA\nEMPATE!')
+	elif player_Option == 1:
+		print('Computador: PEDRA\nVocê: PAPEL\nVOCÊ GANHOU!')
+	elif player_Option == 2:
+		print('Computador: PEDRA\nVocê: TESOURA\nCOMPUTADOR GANHOU!')
+	else:
+		print('Opção invalida!')
 
-# Caso o jogador escolha tesoura
+elif comp_Option == 'Papel':
+	if player_Option == 0:
+		print('Computador: PAPEL\nVocê: PEDRA\nCOMPUTADOR GANHOU!')
+	elif player_Option == 1:
+		print('Computador: PAPEL\nVocê: PAPEL\nEMPATE!')
+	elif player_Option == 2:
+		print('Computador: PAPEL\nVocê: TESOURA\nVOCÊ GANHOU!')
+	else:
+		print('Opção invalida!')
 
-if player_Option == 2 and comp_Option == 0:
-	print('Computador: PEDRA')
-	print('Você: TESOURA')
-	print('Você perdeu, até a próxima!')
-elif player_Option == 2 and comp_Option == 1:
-	print('Computador: TESOURA')
-	print('Você: TESOURA')
-	print('Empate! Até a próxima!')
-elif player_Option == 2 and comp_Option == 2:
-	print('Computador: PAPEL')
-	print('Você: TESOURA')
-	print('Você ganhou! Até a próxima!')
-
-# Caso o jogador escolha papel
-if player_Option == 1 and comp_Option == 0:
-	print('Computador: PEDRA')
-	print('Você: PAPEL')
-	print('Você ganhou! Até a próxima!')
-elif player_Option == 1 and comp_Option == 2:
-	print('Computador: TESOURA')
-	print('Você: PAPEL')
-	print('Você perdeu! Até a próxima!')
-elif player_Option == 1 and comp_Option == 1:
-	print('Computador: PAPEL')
-	print('Você: PAPEL')
-	print('Empate! Até a próxima!')
+elif comp_Option == 'Tesoura':
+	if player_Option == 0:
+		print('Computador: TESOURA\nVocê: PEDRA\nVOCÊ GANHOU!')
+	elif player_Option == 1:
+		print('Computador: TESOURA\nVocê: PAPEL\nCOMPUTADOR GANHOU!')
+	elif player_Option == 2:
+		print('Computador: TESOURA\nVocê: TESOURA\nEMPATE')
+	else:
+		print('Opção inválida!')
 
