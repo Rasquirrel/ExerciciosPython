37aefacdae0a02ccb741c952c9fd50895a479a01
Jose Isac <jose-0A4@protonmail.com>
2022-07-28 12:01:12 -0300

    Terminei de fazer uma parte do progama

--- MundoDois/ex045.py
+++ MundoDois/ex045.py
@@ -5,9 +5,37 @@
 # É um dos primeiros progamas em python que eu desenvolvo enquanto aprendo
 # a utilizar o editor de textos para terminal, VIM.
 
+from random import randint
+from time import sleep
+
 print('-=' * 40)
 print('DESAFIO N°45')
 print('Desenvolva o jogo Jokenpo')
 print('-=' * 40)
 print()
+
+print('BEM VINDO AO JOGO DE PEDRA PAPEL TESOURA!')
+print('Vamos jogar!')
+
+comp_Option = randint(0, 2)
+
+print('Vai!')
+print('''[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura
+''')
+
+player_Option = int(input())
+
+print('Pedra')
+sleep(1.9)
+print('Papel')
+sleep(1)
+print('Tesoura!')
+print()
+
+if player_Option == 0 and comp_Option == 0:
+	print('Computador: PEDRA')
+	print('Você: PEDRA')
+	print('Empate! Até a próxima!')
 
