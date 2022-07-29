ea3e14fa4ab8d3d759d0a6e44662be3407b838b0
Jose Isac <jose-0A4@protonmail.com>
2022-07-29 14:25:14 -0300

    🐿 Ex047 - V1.0
    Terminei o código e aqui ja está a versão 1.0

---
+++ MundoDois/ex047.py
@@ -0,0 +1,8 @@
+# Crie um progama que mostre na tela todos os números pares entre 1 e 50
+# Progama desenvolvido por José Isac
+# 29 de julho, 14:22 V1.0
+
+print('Os números pares entre 1 e 50 são:')
+
+for c in range(0, 51, 2):
+    print(c)
