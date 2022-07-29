275ed15ca67a6f97bf26a54b197dc410b71b159e
Jose Isac <jose-0A4@protonmail.com>
2022-07-29 14:55:12 -0300

    Ex048 - Adicionei o que faltava

--- MundoDois/ex048.py
+++ MundoDois/ex048.py
@@ -5,8 +5,10 @@ que são multiplos de três e que se encontram entre 1 e 500
 # Progama desenvolvido por José Isac
 # 29 de julho, 14:28
 
+soma = 0
 print('Os números ímpares múltiplos de três entre 1 e 500 são:')
 for c in range(3, 499, 3):
     if c % 2 == 1:
         print(c)
-        
+        soma += c
+print('A soma deles é: ', soma)
